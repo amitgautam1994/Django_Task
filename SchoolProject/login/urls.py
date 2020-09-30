b5e97ee33f7f5a35b03acfe5ffd76bd4ede04485
amitgautam1994 <amitgautam1994@gmail.com>
2020-09-30 09:10:42 +0530

    Update urls.py

---
+++ SchoolProject/login/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('auth', views.authenticate_user, name="login"),
+    path('for_pass/', views.forgot_password),  # enter email so that a verification mail can be sent
+    path('auth_for_pass/', views.auth_for_pass.as_view()),  # auth the user
+    path('enter_pass/', views.enter_pass.as_view()),  # enter password to update it
+
+]
